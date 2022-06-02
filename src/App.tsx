1eac480beffd2834373bac28b0a9eaf95664d370
slogup-beobsa <vi2920va@gmail.com>
2022-06-02 16:42:18 +0900

    Fix - IntersectionObsever ing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Observer from './pages/Obsever';
 import Polestat from './pages/Polestat';
+import { PATH } from './shared/enums';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Polestat />} />
+        <Route path={PATH.HOME} element={<Polestat />} />
+        <Route path={PATH.OBSERVER} element={<Observer />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/shared/enums/index.ts
@@ -0,0 +1,4 @@
+export enum PATH {
+  HOME = '/',
+  OBSERVER = '/observer',
+}
